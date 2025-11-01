ea3aa4fc61044f513b78ccfcd398bc52436d0ba9
github-ci <you@example.com>
2025-11-02 03:18:58 +0800

    update++++++

--- tests/Request/OAuth2/GetUserInfoRequestTest.php
+++ tests/Request/OAuth2/GetUserInfoRequestTest.php
@@ -2,7 +2,7 @@
 
 namespace Tourze\LarkOAuth2LoginBundle\Tests\Request\OAuth2;
 
-use HttpClientBundle\Tests\Request\RequestTestCase;
+use HttpClientBundle\Test\RequestTestCase;
 use PHPUnit\Framework\Attributes\CoversClass;
 use Tourze\LarkOAuth2LoginBundle\Request\OAuth2\GetUserInfoRequest;
 
